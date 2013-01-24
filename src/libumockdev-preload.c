fb62f00025bd0ca251122b83807b359dffd087b8
Martin Pitt <martinpitt@gnome.org>
2013-01-24 08:08:59 +0100

    Reorganize code in preload library
    Move the file access wrappers to the bottom, as they need to access the more
    specific wrappers such as sockets and ioctls.

--- src/libumockdev-preload.c
+++ src/libumockdev-preload.c
@@ -48,55 +48,17 @@
 int ioctl_capture_fd = -1;
 FILE* ioctl_capture_log;
 
-static void ioctl_capture_open(int fd);
-
 
 /********************************
  *
- * Wrappers for accessing /sys
+ * Utility functions
  *
  ********************************/
-
-static void *nextlib;
-
-static const char *trap_path(const char *path)
-{
-	static char buf[PATH_MAX * 2];
-	const char *prefix;
-	size_t path_len, prefix_len;
-	int check_exist = 0;
-
-	/* do we need to trap this path? */
-	if (path == NULL)
-		return path;
-
-	prefix = getenv("UMOCKDEV_DIR");
-	if (prefix == NULL)
-		return path;
-
-	if (strncmp(path, "/dev/", 5) == 0)
-		check_exist = 1;
-	else if (strncmp(path, "/sys/", 5) != 0)
-		return path;
-
-	path_len = strlen(path);
-	prefix_len = strlen(prefix);
-	if (path_len + prefix_len >= sizeof(buf)) {
-		errno = ENAMETOOLONG;
-		return NULL;
-	}
-	strcpy(buf, prefix);
-	strcpy(buf + prefix_len, path);
-
-	if (check_exist && access(buf, F_OK) < 0)
-		return path;
-
-	return buf;
-}
 
 static void *get_libc_func(const char *f)
 {
 	void *fp;
+	static void *nextlib;
 
 	if (nextlib == NULL) {
 #ifdef RTLD_NEXT
@@ -112,102 +74,6 @@ static void *get_libc_func(const char *f)
 	return fp;
 }
 
-/* wrapper template for a function with one "const char* path" argument */
-#define WRAP_1ARG(rettype, failret, name) \
-rettype name(const char *path) \
-{ \
-	const char *p;				\
-	static rettype (*_fn)(const char*);	\
-	_fn = get_libc_func(#name);		\
-	p = trap_path(path);			\
-	if (p == NULL)				\
-		return failret;			\
-	return (*_fn)(p);			\
-}
-
-/* wrapper template for a function with "const char* path" and another argument */
-#define WRAP_2ARGS(rettype, failret, name, arg2t) \
-rettype name(const char *path, arg2t arg2) \
-{ \
-	const char *p;					\
-	static rettype (*_fn)(const char*, arg2t arg2);	\
-	_fn = get_libc_func(#name);			\
-	p = trap_path(path);				\
-	if (p == NULL)					\
-		return failret;				\
-	return (*_fn)(p, arg2);				\
-}
-
-/* wrapper template for a function with "const char* path" and two other arguments */
-#define WRAP_3ARGS(rettype, failret, name, arg2t, arg3t) \
-rettype name(const char *path, arg2t arg2, arg3t arg3) \
-{ \
-	const char *p;						    \
-	static rettype (*_fn)(const char*, arg2t arg2, arg3t arg3); \
-	_fn = get_libc_func(#name);				    \
-	p = trap_path(path);					    \
-	if (p == NULL)						    \
-		return failret;					    \
-	return (*_fn)(p, arg2, arg3);				    \
-}
-
-/* wrapper template for __xstat family */
-#define WRAP_VERSTAT(prefix, suffix) \
-int prefix ## stat ## suffix (int ver, const char *path, struct stat ## suffix *st) \
-{ \
-	const char *p;								    \
-	static int (*_fn)(int ver, const char *path, struct stat ## suffix *buf);   \
-	_fn = get_libc_func(#prefix "stat" #suffix);				    \
-	p = trap_path(path);							    \
-        /* printf("testbed wrapped " #prefix "stat" #suffix "(%s) -> %s\n", path, p);*/	\
-	if (p == NULL)								    \
-		return -1;							    \
-	return _fn(ver, p, st);							    \
-}
-
-/* wrapper template for open family */
-#define WRAP_OPEN(prefix, suffix) \
-int prefix ## open ## suffix (const char *path, int flags, ...)	    \
-{ \
-	const char *p;						    \
-	static int (*_fn)(const char *path, int flags, ...);	    \
-	int ret;						    \
-	_fn = get_libc_func(#prefix "open" #suffix);		    \
-	p = trap_path(path);					    \
-	if (p == NULL)						    \
-		return -1;					    \
-	if (flags & O_CREAT) {					    \
-		mode_t mode;					    \
-		va_list ap;					    \
-		va_start(ap, flags);				    \
-		mode = va_arg(ap, mode_t);			    \
-		va_end(ap);					    \
-		ret = _fn(p, flags, mode);			    \
-	} else							    \
-		ret = _fn(p, flags);				    \
-	ioctl_capture_open(ret);				    \
-	return ret;						    \
-}
-
-WRAP_1ARG(DIR*, NULL, opendir);
-
-WRAP_2ARGS(FILE*, NULL, fopen, const char*);
-WRAP_2ARGS(int, -1, mkdir, mode_t);
-WRAP_2ARGS(int, -1, access, int);
-WRAP_2ARGS(int, -1, stat, struct stat*);
-WRAP_2ARGS(int, -1, stat64, struct stat64*);
-WRAP_2ARGS(int, -1, lstat, struct stat*);
-WRAP_2ARGS(int, -1, lstat64, struct stat64*);
-
-WRAP_3ARGS(ssize_t, -1, readlink, char*, size_t);
-
-WRAP_VERSTAT(__x,);
-WRAP_VERSTAT(__x,64);
-WRAP_VERSTAT(__lx,);
-WRAP_VERSTAT(__lx,64);
-
-WRAP_OPEN(,);
-WRAP_OPEN(,64);
 
 /********************************
  *
@@ -297,20 +163,6 @@ int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 	}
 
 	return _bind(sockfd, addr, addrlen);
-}
-
-int close(int fd)
-{
-	static int (*_close)(int);
-	_close = get_libc_func("close");
-	if (is_wrapped_socket(fd)) {
-		/* printf("testbed wrapped close: closing netlink socket fd %i\n", fd); */
-		unmark_wrapped_socket(fd);
-	}
-	if (fd == ioctl_capture_fd)
-		ioctl_capture_fd = -1;
-
-	return _close(fd);
 }
 
 ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags)
@@ -460,6 +312,159 @@ ioctl(int d, unsigned long request, void *arg)
 		capture_ioctl (request, arg);
 
 	return result;
+}
+
+
+/********************************
+ *
+ * Wrappers for accessing files
+ *
+ ********************************/
+
+static const char *trap_path(const char *path)
+{
+	static char buf[PATH_MAX * 2];
+	const char *prefix;
+	size_t path_len, prefix_len;
+	int check_exist = 0;
+
+	/* do we need to trap this path? */
+	if (path == NULL)
+		return path;
+
+	prefix = getenv("UMOCKDEV_DIR");
+	if (prefix == NULL)
+		return path;
+
+	if (strncmp(path, "/dev/", 5) == 0)
+		check_exist = 1;
+	else if (strncmp(path, "/sys/", 5) != 0)
+		return path;
+
+	path_len = strlen(path);
+	prefix_len = strlen(prefix);
+	if (path_len + prefix_len >= sizeof(buf)) {
+		errno = ENAMETOOLONG;
+		return NULL;
+	}
+	strcpy(buf, prefix);
+	strcpy(buf + prefix_len, path);
+
+	if (check_exist && access(buf, F_OK) < 0)
+		return path;
+
+	return buf;
+}
+
+/* wrapper template for a function with one "const char* path" argument */
+#define WRAP_1ARG(rettype, failret, name) \
+rettype name(const char *path) \
+{ \
+	const char *p;				\
+	static rettype (*_fn)(const char*);	\
+	_fn = get_libc_func(#name);		\
+	p = trap_path(path);			\
+	if (p == NULL)				\
+		return failret;			\
+	return (*_fn)(p);			\
+}
+
+/* wrapper template for a function with "const char* path" and another argument */
+#define WRAP_2ARGS(rettype, failret, name, arg2t) \
+rettype name(const char *path, arg2t arg2) \
+{ \
+	const char *p;					\
+	static rettype (*_fn)(const char*, arg2t arg2);	\
+	_fn = get_libc_func(#name);			\
+	p = trap_path(path);				\
+	if (p == NULL)					\
+		return failret;				\
+	return (*_fn)(p, arg2);				\
+}
+
+/* wrapper template for a function with "const char* path" and two other arguments */
+#define WRAP_3ARGS(rettype, failret, name, arg2t, arg3t) \
+rettype name(const char *path, arg2t arg2, arg3t arg3) \
+{ \
+	const char *p;						    \
+	static rettype (*_fn)(const char*, arg2t arg2, arg3t arg3); \
+	_fn = get_libc_func(#name);				    \
+	p = trap_path(path);					    \
+	if (p == NULL)						    \
+		return failret;					    \
+	return (*_fn)(p, arg2, arg3);				    \
+}
+
+/* wrapper template for __xstat family */
+#define WRAP_VERSTAT(prefix, suffix) \
+int prefix ## stat ## suffix (int ver, const char *path, struct stat ## suffix *st) \
+{ \
+	const char *p;								    \
+	static int (*_fn)(int ver, const char *path, struct stat ## suffix *buf);   \
+	_fn = get_libc_func(#prefix "stat" #suffix);				    \
+	p = trap_path(path);							    \
+        /* printf("testbed wrapped " #prefix "stat" #suffix "(%s) -> %s\n", path, p);*/	\
+	if (p == NULL)								    \
+		return -1;							    \
+	return _fn(ver, p, st);							    \
+}
+
+/* wrapper template for open family */
+#define WRAP_OPEN(prefix, suffix) \
+int prefix ## open ## suffix (const char *path, int flags, ...)	    \
+{ \
+	const char *p;						    \
+	static int (*_fn)(const char *path, int flags, ...);	    \
+	int ret;						    \
+	_fn = get_libc_func(#prefix "open" #suffix);		    \
+	p = trap_path(path);					    \
+	if (p == NULL)						    \
+		return -1;					    \
+	if (flags & O_CREAT) {					    \
+		mode_t mode;					    \
+		va_list ap;					    \
+		va_start(ap, flags);				    \
+		mode = va_arg(ap, mode_t);			    \
+		va_end(ap);					    \
+		ret = _fn(p, flags, mode);			    \
+	} else							    \
+		ret = _fn(p, flags);				    \
+	ioctl_capture_open(ret);				    \
+	return ret;						    \
+}
+
+WRAP_1ARG(DIR*, NULL, opendir);
+
+WRAP_2ARGS(FILE*, NULL, fopen, const char*);
+WRAP_2ARGS(int, -1, mkdir, mode_t);
+WRAP_2ARGS(int, -1, access, int);
+WRAP_2ARGS(int, -1, stat, struct stat*);
+WRAP_2ARGS(int, -1, stat64, struct stat64*);
+WRAP_2ARGS(int, -1, lstat, struct stat*);
+WRAP_2ARGS(int, -1, lstat64, struct stat64*);
+
+WRAP_3ARGS(ssize_t, -1, readlink, char*, size_t);
+
+WRAP_VERSTAT(__x,);
+WRAP_VERSTAT(__x,64);
+WRAP_VERSTAT(__lx,);
+WRAP_VERSTAT(__lx,64);
+
+WRAP_OPEN(,);
+WRAP_OPEN(,64);
+
+int close(int fd)
+{
+	static int (*_close)(int);
+	_close = get_libc_func("close");
+	if (is_wrapped_socket(fd)) {
+		/* printf("testbed wrapped close: closing netlink socket fd %i\n", fd); */
+		unmark_wrapped_socket(fd);
+	}
+	if (fd == ioctl_record_fd)
+		ioctl_record_fd = -1;
+
+	return _close(fd);
 }
 
 /* vim: set sw=8 noet: */
