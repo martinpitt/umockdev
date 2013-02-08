martinpitt/umockdev

commit 4fc177fc59f6a583c3e96a6ef6ac6b424bff6481

Move hw/state independent ioctls to ioctl-tree.c
This gets rid of all ioctl specifics in libumockdev-preload.c.

src/ioctl_tree.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <errno.h>
 #include <sys/ioctl.h>
 #include <linux/usbdevice_fs.h>
 
@@ -305,11 +306,27 @@ ioctl_tree*
 ioctl_tree_execute (ioctl_tree* tree, ioctl_tree *last, unsigned long id,
                     void* arg, int* ret)
 {
-    ioctl_tree *i = ioctl_tree_next_wrap (tree, last);
+    const ioctl_type *t;
+    ioctl_tree *i;
     int r, handled;
 
     DBG ("ioctl_tree_execute ioctl %lX\n", id);
 
+    /* check if it's a hardware independent stateless ioctl */
+    t = ioctl_type_get_by_id (id);
+    if (t != NULL && t->insertion_parent == NULL) {
+        DBG ("  ioctl_tree_execute: stateless\n");
+        if (t->execute (NULL, id, arg, &r))
+            *ret = r;
+        else
+            *ret = -1;
+        return last;
+    }
+
+    if (tree == NULL)
+        return NULL;
+
+    i = ioctl_tree_next_wrap (tree, last);
     /* start at the previously executed node to maintain original order of
      * ioctls as much as possible (i. e. maintain it while the requests come in
      * at the same order as originally recorded) */
@@ -622,6 +639,36 @@ usbdevfs_reapurb_insertion_parent (ioctl_tree* tree,
 
 /***********************************
  *
+ * generic implementations for hardware/state independent ioctls
+ *
+ ***********************************/
+
+static int
+ioctl_execute_success (const ioctl_tree* node, unsigned long id, void* arg, int *ret)
+{
+    errno = 0;
+    *ret = 0;
+    return 1;
+}
+
+static int
+ioctl_execute_enodata (const ioctl_tree* node, unsigned long id, void* arg, int *ret)
+{
+    errno = ENODATA;
+    *ret = -1;
+    return 1;
+}
+
+static int
+ioctl_execute_enotty (const ioctl_tree* node, unsigned long id, void* arg, int *ret)
+{
+    errno = ENOTTY;
+    *ret = -1;
+    return 1;
+}
+
+/***********************************
+ *
  * Known ioctls
  *
  ***********************************/
@@ -641,6 +688,23 @@ ioctl_type ioctl_db[] = {
       usbdevfs_reapurb_init_from_bin, usbdevfs_reapurb_init_from_text,
       usbdevfs_reapurb_write, usbdevfs_reapurb_equal,
       usbdevfs_reapurb_execute, usbdevfs_reapurb_insertion_parent },
+
+    /* hardware/state independent ioctls */
+    { USBDEVFS_CLAIMINTERFACE, "USBDEVFS_CLAIMINTERFACE",
+      NULL, NULL, NULL, NULL, ioctl_execute_success, NULL },
+    { USBDEVFS_RELEASEINTERFACE, "USBDEVFS_RELEASEINTERFACE",
+      NULL, NULL, NULL, NULL, ioctl_execute_success, NULL },
+    { USBDEVFS_CLEAR_HALT, "USBDEVFS_CLEAR_HALT",
+      NULL, NULL, NULL, NULL, ioctl_execute_success, NULL },
+    { USBDEVFS_RESET, "USBDEVFS_RESET",
+      NULL, NULL, NULL, NULL, ioctl_execute_success, NULL },
+    { USBDEVFS_RESETEP, "USBDEVFS_RESETEP",
+      NULL, NULL, NULL, NULL, ioctl_execute_success, NULL },
+    { USBDEVFS_GETDRIVER, "USBDEVFS_GETDRIVER",
+      NULL, NULL, NULL, NULL, ioctl_execute_enodata, NULL },
+    { USBDEVFS_IOCTL, "USBDEVFS_IOCTL",
+      NULL, NULL, NULL, NULL, ioctl_execute_enotty, NULL },
+
     /* terminator */
     { 0, "", NULL, NULL, NULL, NULL, NULL }
 };

Makefile.am
@@ -49,6 +49,7 @@ AM_CFLAGS = 	\
 	-Werror=missing-include-dirs	\
 	-Werror=unused-variable		\
 	-Werror=return-type		\
+	-Werror=uninitialized		\
 	-Wall
 
 # -------------------------------------------------------------

src/libumockdev-preload.c
@@ -39,8 +39,6 @@
 #include <sys/socket.h>
 #include <linux/un.h>
 #include <linux/netlink.h>
-#include <linux/ioctl.h>
-#include <linux/usbdevice_fs.h>
 #include <unistd.h>
 
 #include "ioctl_tree.h"
@@ -335,39 +333,15 @@ static int
 ioctl_emulate(unsigned long request, void *arg)
 {
 	ioctl_tree *ret;
-	int ioctl_result;
-
-	switch (request) {
-	    /* hw/operation independent USBDEVFS management requests */
-	    case USBDEVFS_CLAIMINTERFACE:
-	    case USBDEVFS_RELEASEINTERFACE:
-	    case USBDEVFS_CLEAR_HALT:
-	    case USBDEVFS_RESET:
-	    case USBDEVFS_RESETEP:
-		errno = 0;
-	        return 0;
-
-	    case USBDEVFS_GETDRIVER:
-		errno = ENODATA;
-		return -1;
-
-	    case USBDEVFS_IOCTL:
-		errno = ENOTTY;
-		return -1;
-	}
+	int ioctl_result = -2;
 
 	/* check our ioctl tree */
-	if (ioctl_record != NULL) {
-		ret = ioctl_tree_execute (ioctl_record, ioctl_last, request, arg, &ioctl_result);
-		if (ret != NULL) {
-			ioctl_last = ret;
-			return ioctl_result;
-		}
-	}
+	ret = ioctl_tree_execute (ioctl_record, ioctl_last, request, arg, &ioctl_result);
+	if (ret != NULL)
+		ioctl_last = ret;
 
-
-	/* means "unhandled" */
-	return -2;
+	/* -2 means "unhandled" */
+	return ioctl_result;
 }
 
 

commit 05fab7ff58fb1dbd5d1d24b06e195c00183093c6

Fix ioctl_tree_new_from_bin() crash for state independent ioctls
Regression from 4fc177fc59f6a5.

src/ioctl_tree.c
@@ -55,6 +55,10 @@ ioctl_tree_new_from_bin (unsigned long id, const void* data)
         DBG ("ioctl_tree_new_from_bin: unknown ioctl %lX\n", id);
         return NULL;
     }
+    /* state independent ioctl? */
+    if (type->init_from_bin == NULL)
+        return NULL;
+
     t = calloc(sizeof (ioctl_tree), 1);
     t->type = type;
     type->init_from_bin (t, data);